d9b9eed121e0c96adc5cd2aa44b8ebd47b054e08
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-09-02 11:43:30 +0200

    Conditional interface tester per interface, filling extra relations

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -1,22 +1,60 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
-from unittest.mock import patch
 
 import pytest
-from charms.tempo_k8s.v1.charm_tracing import charm_tracing_disabled
+from cosl.coordinated_workers.interface import ClusterRequirerUnitData, ClusterRequirerAppData
 from interface_tester import InterfaceTester
 from ops.pebble import Layer
-from scenario.state import Container, State
-from unittest.mock import MagicMock
+from scenario import Relation
+from scenario.state import Container, State, PeerRelation
 
 from charm import TempoCoordinatorCharm
+from charms.tempo_k8s.v1.charm_tracing import charm_tracing_disabled
 
-s3_connection = MagicMock(return_value={
+tempo_container = Container(
+    name="tempo",
+    can_connect=True,
+    layers={
+        "foo": Layer(
+            {
+                "summary": "foo",
+                "description": "bar",
+                "services": {
+                    "tempo": {
+                        "startup": "enabled",
+                        "current": "active",
+                        "name": "tempo",
+                    }
+                },
+                "checks": {},
+            }
+        )
+    },
+)
+
+s3_relation = Relation("s3", remote_app_data={
+    "access-key": "key",
     "bucket": "tempo",
-    "endpoint": "http://mock.s3.com",
-    "access-key": "accesskey",
-    "secret-key": "secretkey",
+    "endpoint": "http://1.2.3.4:9000",
+    "secret-key": "soverysecret",
 })
+cluster_relation = Relation(
+    "cluster",
+    remote_app_data=ClusterRequirerAppData(role="all").dump(),
+    remote_units_data={
+        0: ClusterRequirerUnitData(
+            address="http://example.com",
+            juju_topology={
+                "application": "app",
+                "unit": "unit",
+                "charm_name": "charmname"
+
+            }).dump()
+    }
+)
+
+peers = PeerRelation("peers", peers_data={1: {}})
+
 
 # Interface tests are centrally hosted at https://github.com/canonical/charm-relation-interfaces.
 # this fixture is used by the test runner of charm-relation-interfaces to test tempo's compliance
@@ -26,22 +64,34 @@ s3_connection = MagicMock(return_value={
 # to include the new identifier/location.
 @pytest.fixture
 def interface_tester(interface_tester: InterfaceTester):
-    with patch('lib.charms.data_platform_libs.v0.s3.S3Requirer.get_s3_connection_info', new=s3_connection):
-        with charm_tracing_disabled():
+    with charm_tracing_disabled():
+        # FIXME: expose publicly
+        interface_name = interface_tester._interface_name  # noqa
+
+        # if we're testing the tracing interface:
+        if interface_name == "tracing":
             interface_tester.configure(
                 charm_type=TempoCoordinatorCharm,
-                state_template=State(
-                    leader=True,
-                    containers=[
-                        Container(
-                            name="nginx",
-                            can_connect=True,
-                        ),
-                        Container(
-                            name="nginx-prometheus-exporter",
-                            can_connect=True,
-                        )
-                    ],
-                ),
+                state_template=State(leader=True, containers=[tempo_container],
+                                     relations=[peers, s3_relation, cluster_relation]),
             )
             yield interface_tester
+
+        # if we're testing the s3 interface:
+        elif interface_name == "s3":
+            interface_tester.configure(
+                charm_type=TempoCoordinatorCharm,
+                state_template=State(leader=True, containers=[tempo_container], relations=[peers, cluster_relation]),
+            )
+            yield interface_tester
+
+        # if we're testing the cluster interface:
+        elif interface_name == "cluster":
+            interface_tester.configure(
+                charm_type=TempoCoordinatorCharm,
+                state_template=State(leader=True, containers=[tempo_container], relations=[peers, s3_relation]),
+            )
+            yield interface_tester
+
+        else:
+            raise ValueError(f"testing {interface_name} not supported")
