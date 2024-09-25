0d98fe6ad36a47cfc1675c0261e81b0c90435c41
michael <michael.dmitry98@gmail.com>
2024-09-25 16:25:11 +0300

    add terraform module

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default = "tempo
+}
+
+variable "channel" {
+  description = "Charm channel"
+  type        = string
+  default     = "latest/edge"
+}
+
+
+variable "config" {
+  description = "Charm config options as in the ones we pass in juju config"
+  type        = map(any)
+  default     = {}
+}
+
+variable "model_name" {
+  description = "Model name"
+  type        = string
+}
+
+variable "revision" {
+  description = "Charm revision"
+  type        = number
+  default     = null
+}
+
+variable "units" {
+  description = "Number of units"
+  type        = number
+  default     = 1
+}
