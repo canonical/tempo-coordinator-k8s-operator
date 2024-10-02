3c3eb21a49d12e7ec6f12946ab200ee4d9fec2ae
Pietro Pasotti <starfire.daemon@gmail.com>
2024-10-02 08:28:11 +0200

    clean

--- dev-release.sh
+++ dev-release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+charmcraft clean
 CHARM=$(charmcraft pack --format json | jq ".charms[0]" | tr -d '"')
 REV=$(charmcraft upload $CHARM --format json | jq ".revision")
 charmcraft release tempo-coordinator-k8s -r $REV --resource nginx-image:3 --resource nginx-prometheus-exporter-image:3 --channel latest/edge/dev
