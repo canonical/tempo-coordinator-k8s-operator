c728f29bb998feb0dc61e5f652f26f40993ccb9e
PietroPasotti <pietro.pasotti@canonical.com>
2024-12-02 13:12:42 +0100

    Revert "[DO NOT MERGE] Grafana datasource interface tests (#82)"
    This reverts commit d59648168fd0b7fd5d2c59a73555b84959cba709.

--- tests/interface/test_tempo_cluster.py
+++ tests/interface/test_tempo_cluster.py
@@ -3,9 +3,9 @@
 from interface_tester import InterfaceTester
 
 
-def test_tempo_cluster_v0_interface(cluster_tester: InterfaceTester):
-    cluster_tester.configure(
+def test_tempo_cluster_v0_interface(interface_tester: InterfaceTester):
+    interface_tester.configure(
         interface_name="tempo_cluster",
         interface_version=0,
     )
-    cluster_tester.run()
+    interface_tester.run()
--- tests/interface/test_tracing.py
+++ tests/interface/test_tracing.py
@@ -3,9 +3,9 @@
 from interface_tester import InterfaceTester
 
 
-def test_tracing_v2_interface(tracing_tester: InterfaceTester):
-    tracing_tester.configure(
+def test_tracing_v2_interface(interface_tester: InterfaceTester):
+    interface_tester.configure(
         interface_name="tracing",
         interface_version=2,
     )
-    tracing_tester.run()
+    interface_tester.run()
