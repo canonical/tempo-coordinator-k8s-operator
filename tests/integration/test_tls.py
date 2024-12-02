98576f82ae076c38a814821f81b03bd2e9edf9df
Pietro Pasotti <starfire.daemon@gmail.com>
2024-12-02 09:04:01 +0100

    fixed async test

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -156,11 +156,10 @@ def test_verify_traces_force_enabled_protocols_tls(nonce, protocol, juju):
     get_traces_patiently(tempo_host, service_name=f"tracegen-{protocol}")
 
 
-@pytest.mark.abort_on_fail
-async def test_workload_traces_tls(ops_test: OpsTest):
-    tempo_host = await get_ingress_proxied_hostname(ops_test)
+def test_workload_traces_tls(juju):
+    tempo_host = get_ingress_proxied_hostname(juju)
     # verify traces from tempo-scalable-single-binary are ingested
-    assert await get_traces_patiently(
+    get_traces_patiently(
         tempo_host,
         service_name="tempo-scalable-single-binary",
     )
