0829c80a2b9aa457e4be93f0d8c0cb5c58697dbc
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-04-29 12:22:07 +0200

    catch error on charm-tracing-config if leader databag is empty

--- tests/scenario/test_tracing_provider.py
+++ tests/scenario/test_tracing_provider.py
@@ -1,17 +1,20 @@
-from charms.tempo_coordinator_k8s.v0.charm_tracing import charm_tracing_disabled
-from charms.tempo_coordinator_k8s.v0.tracing import TracingProviderAppData
+import pytest
 from scenario import Relation, State
 
+from charms.tempo_coordinator_k8s.v0.charm_tracing import charm_tracing_disabled
+from charms.tempo_coordinator_k8s.v0.tracing import TracingProviderAppData
 
-def test_receivers_removed_on_relation_broken(
-    context, s3, all_worker, nginx_container, nginx_prometheus_exporter_container
+
+@pytest.mark.parametrize("leader", (True, False))
+def test_receiver_api(
+        context, s3, all_worker, nginx_container, nginx_prometheus_exporter_container, leader
 ):
     tracing_grpc = Relation(
         "tracing",
         remote_app_data={"receivers": '["otlp_grpc"]'},
         local_app_data={
             "receivers": '[{"protocol": {"name": "otlp_grpc", "type": "grpc"} , "url": "foo.com:10"}, '
-            '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}, '
+                         '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}] '
         },
     )
     tracing_http = Relation(
@@ -19,7 +22,47 @@ def test_receivers_removed_on_relation_broken(
         remote_app_data={"receivers": '["otlp_http"]'},
         local_app_data={
             "receivers": '[{"protocol": {"name": "otlp_grpc", "type": "grpc"} , "url": "foo.com:10"}, '
-            '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}, '
+                         '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}] '
+        },
+    )
+
+    state = State(
+        leader=leader,
+        relations=[tracing_grpc, tracing_http, s3, all_worker],
+        containers=[nginx_container, nginx_prometheus_exporter_container],
+    )
+
+    with charm_tracing_disabled():
+        # WHEN any event occurs
+        with context(context.on.update_status(), state) as mgr:
+            charm = mgr.charm
+            assert charm._requested_receivers() == ("otlp_grpc", "otlp_http")
+            state_out = mgr.run()
+
+    r_out = [r for r in state_out.relations if r.id == tracing_http.id][0]
+    # "otlp_grpc" is gone from the databag
+    assert sorted(
+        [r.protocol.name for r in TracingProviderAppData.load(r_out.local_app_data).receivers]
+    ) == ["otlp_grpc", "otlp_http"]
+
+
+def test_leader_removes_receivers_on_relation_broken(
+        context, s3, all_worker, nginx_container, nginx_prometheus_exporter_container
+):
+    tracing_grpc = Relation(
+        "tracing",
+        remote_app_data={"receivers": '["otlp_grpc"]'},
+        local_app_data={
+            "receivers": '[{"protocol": {"name": "otlp_grpc", "type": "grpc"} , "url": "foo.com:10"}, '
+                         '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}] '
+        },
+    )
+    tracing_http = Relation(
+        "tracing",
+        remote_app_data={"receivers": '["otlp_http"]'},
+        local_app_data={
+            "receivers": '[{"protocol": {"name": "otlp_grpc", "type": "grpc"} , "url": "foo.com:10"}, '
+                         '{"protocol": {"name": "otlp_http", "type": "http"}, "url": "http://foo.com:11"}] '
         },
     )
 
